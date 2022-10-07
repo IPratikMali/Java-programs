c87ed61aa84ecce7d76bc05ade7121548d8ee0f5
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-10-07 19:04:29 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,23 @@
+//write a program to print 5 stars.
+
+public class pattern2 {
+
+    public static void main(String[] args){
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.println("*");
+        }
+    }
+}
+
+
+/* ouput
+
+ *
+ *
+ *
+ *
+ *
+
+ */
