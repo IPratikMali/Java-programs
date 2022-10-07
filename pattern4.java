1ba39093884b4e81d699b0fe187fd541d1a418dc
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-10-07 19:04:50 +0530

    Add files via upload

---
+++ pattern4.java
@@ -0,0 +1,21 @@
+//write a program to print 5 stars 5 times in the same row.
+
+public class pattern4 {
+
+    public static void main(String[] args){
+
+        for(int j = 1 ; j<=5 ; j++)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                System.out.print("*");
+            }
+        }
+    }
+}
+
+/* output
+
+ *************************
+
+ */
