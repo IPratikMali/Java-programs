915e152fe82eaf777d9e0bae2978923886eb681b
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-10-07 19:05:21 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,22 @@
+// write a program to print parallelogram tilted towards right.
+
+public class pattern7 {
+
+    public static void main(String[] args){
+
+        for(int j = 1 ; j<=5 ; j++)
+        {
+             for(int k = 1 ; k <= j ; k++)
+            {
+                System.out.print("");
+                for (int i = 1; i <= 5; i++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+
+        }
+    }
+}
