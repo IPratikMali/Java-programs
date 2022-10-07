c05944e27a40da326e350574994333b4590a6acf
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-10-07 19:04:40 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,18 @@
+//write a program to print 5 stars in the same line.
+
+public class pattern3 {
+
+    public static void main(String[] args) {
+
+        for(int i = 1 ; i <= 5 ; i++)
+        {
+            System.out.print("*");
+        }
+    }
+}
+
+/*output
+
+ *****
+
+ */
