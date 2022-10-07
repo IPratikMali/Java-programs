65bdea6ea716e1548c7986aa0c76937e5790a2aa
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-10-07 19:05:10 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,27 @@
+//write a program to print stars in rectangle shape .
+
+public class pattern6 {
+
+    public static void main(String[] args){
+
+        for(int j =1 ; j<=4 ;j++)
+        {
+            System.out.println();
+
+            for (int i = 1; i <= 7; i++)
+            {
+                System.out.print("*  ");
+
+            }
+        }
+    }
+}
+
+/* output
+
+ *  *  *  *  *  *  *
+ *  *  *  *  *  *  *
+ *  *  *  *  *  *  *
+ *  *  *  *  *  *  *
+
+ */
