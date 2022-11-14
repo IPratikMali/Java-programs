8cf07651a60da869c11ddc8dc58d2b00821593de
Pratik Mali <100037271+IPratikMali@users.noreply.github.com>
2022-11-14 19:13:39 +0530

    Add files via upload

---
+++ pattern8.java
@@ -0,0 +1,16 @@
+
+public class pattern8 {
+
+    public static void main(String args[]) {
+        for (int j = 5; j > 0; j--) {
+            for (int i = 0; i < 5-j; i++) {
+                int d = 1;
+                System.out.print(d);
+                d++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+
